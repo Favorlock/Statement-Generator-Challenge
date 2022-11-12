6203fb98be594fb102415207b704685e6e9f7242
Evan Lindsay <evanlindsay@live.com>
2022-11-11 18:09:27 -0800

    Load and deserialize input.json to ActionLog model.

--- src/main/java/com/favorlock/challenge/StatementGenerator.java
+++ src/main/java/com/favorlock/challenge/StatementGenerator.java
@@ -1,7 +1,51 @@
 package com.favorlock.challenge;
 
+import com.favorlock.challenge.gson.StockActionAdapter;
+import com.favorlock.challenge.models.ActionLog;
+import com.favorlock.challenge.models.StockAction;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import java.io.IOException;
+import java.io.Reader;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.text.SimpleDateFormat;
+
 public class StatementGenerator {
+    private static StatementGenerator instance;
+
+    private static final SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd");
+
+    private Gson gson;
+
+    public StatementGenerator() {
+        gson = new GsonBuilder()
+                .setDateFormat(dateFormat.toPattern())
+                .registerTypeAdapter(StockAction.class, new StockActionAdapter())
+                .create();
+    }
+
+    public String generateStatement(String path) throws IOException {
+        Reader reader = Files.newBufferedReader(Path.of(path));
+        ActionLog log = gson.fromJson(reader, ActionLog.class);
+
+        // TODO: Implement statement generation.
+
+        return "";
+    }
+
     public static void main(String... args) {
-        // TODO
+        instance = new StatementGenerator();
+
+        String statement;
+
+        try {
+            statement = instance.generateStatement("./input.json");
+        } catch (Exception ex) {
+            throw new RuntimeException("Could not locate input file or contents are corrupt.");
+        }
+
+        // TODO: do something with the generated statement.
     }
 }
