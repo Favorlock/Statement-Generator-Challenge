43457284ecc84b7bc6cbc9e89d2ef84b475a4088
Evan Lindsay <evanlindsay@live.com>
2022-11-11 21:28:50 -0800

    Integrate StatementGenerator with main class.

--- src/main/java/com/favorlock/challenge/Application.java
+++ src/main/java/com/favorlock/challenge/Application.java
@@ -1,12 +1,11 @@
 package com.favorlock.challenge;
 
-import com.favorlock.challenge.gson.StockActionAdapter;
-import com.favorlock.challenge.models.ActionLog;
-import com.favorlock.challenge.models.StockAction;
+import com.favorlock.challenge.gson.StockTransactionAdapter;
+import com.favorlock.challenge.models.transaction.TransactionLog;
+import com.favorlock.challenge.models.transaction.StockTransaction;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
-import java.io.IOException;
 import java.io.Reader;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -15,37 +14,33 @@ import java.text.SimpleDateFormat;
 public class Application {
     private static Application instance;
 
-    private static final SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd");
-
     private Gson gson;
 
     public Application() {
         gson = new GsonBuilder()
-                .setDateFormat(dateFormat.toPattern())
-                .registerTypeAdapter(StockAction.class, new StockActionAdapter())
+                .setDateFormat(new SimpleDateFormat("yyyy/MM/dd HH:mm:ss").toPattern())
+                .registerTypeAdapter(StockTransaction.class, new StockTransactionAdapter())
                 .create();
     }
 
-    public String generateStatement(String path) throws IOException {
-        Reader reader = Files.newBufferedReader(Path.of(path));
-        ActionLog log = gson.fromJson(reader, ActionLog.class);
+    public String generateStatement(String path) {
+        Reader reader;
 
-        // TODO: Implement statement generation.
+        try {
+            reader = Files.newBufferedReader(Path.of(path));
+        } catch (Exception ex) {
+            throw new RuntimeException("Could not locate input file or contents are corrupt.");
+        }
 
-        return "";
+        TransactionLog log = gson.fromJson(reader, TransactionLog.class);
+        StatementGenerator generator = new StatementGenerator(log);
+
+        return generator.toString();
     }
 
     public static void main(String... args) {
         instance = new Application();
 
-        String statement;
-
-        try {
-            statement = instance.generateStatement("./input.json");
-        } catch (Exception ex) {
-            throw new RuntimeException("Could not locate input file or contents are corrupt.");
-        }
-
-        // TODO: do something with the generated statement.
+        System.out.println(instance.generateStatement("./input.json"));
     }
 }
